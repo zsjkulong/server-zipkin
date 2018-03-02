910bb5585af508bad5accd1251cf714eddac2673
TSKY.sijian.zhou <zhousijian@travelsky.com>
2018-03-02 09:39:00 +0800

    Bug:3333 Bugzilla Project: Sichuan Airline Description:

---
+++ src/main/java/com/zsj/server_zipkin/App.java
@@ -0,0 +1,13 @@
+package com.zsj.server_zipkin;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
